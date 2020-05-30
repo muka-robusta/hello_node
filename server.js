a79e784616e222e409a43001db7a3cd9b47cb522
Illia Savenko <illiafun@yandex.ua>
2020-05-30 17:18:05 +0300

    added first page

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ const mimeTypes = {
     "css": "text/css"
 } 
 
-http.createServer(function(req, res) {
+http.createServer(function(req, res){
     var uri = url.parse(req.url).pathname;
     var fileName = path.join(process.cwd(), unescape(uri));
-    console.log("Loading" + uri);
+    console.log("Loading " + uri);
     var stats;
 
     try {
-        stats = fs.lstatSync();
+        stats = fs.lstatSync(fileName);
     }catch(e){
         res.writeHead(404, {"Content-Type": "text/plain"});
         res.write("404 - page not found");
@@ -31,7 +31,7 @@ http.createServer(function(req, res) {
         var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]];
         res.writeHead(200, {"Content-Type": mimeType});
         
-        var fileStream = fs.createReadStream(filename);
+        var fileStream = fs.createReadStream(fileName);
         fileStream.pipe(res);
     } else if(stats.isDirectory())
     {
